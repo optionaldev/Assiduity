5c5bbbb316cd719ea46169dbf929b45f2369ce15
HOME-PC\OD <archyusa@icloud.com>
2021-07-23 23:51:45 +0300

    Added some documentation for groups functionality. Involved specs for persitence

--- src/new_frames/groups.lua
+++ src/new_frames/groups.lua
@@ -22,7 +22,31 @@ local BAR_WIDTH = BUTTON_WIDTH - 2 * DISTANCE_TO_EDGE
 
 -- Main frame that will also handle the events
 AssiduityGroupsFrame = CreateFrame("Frame", AssiduityGroupsFrame, UIParent)
-AssiduityGroupsFrame:Hide()
+--AssiduityGroupsFrame:Hide()
+
+--[[
+	If we already established a player's spec, aside from accounting
+	for spec changes for when players leave or we're about to face
+	Valithria, for the most part, specs don't change.
+	
+	Worth noting that in Druid's case, just because we know he's Feral
+	doesn't mean we know his role. Could still be both tank and mdps.
+	
+	Also, doesn't make sense to add spec to this table if the role
+	doesn't change, such as for mages, hunters, rogues & warlocks.
+]]
+local nameToSpec
+
+local specToRole = {
+
+	["Balance"]	    = "rdps",
+	["Elemental"]   = "rdps",
+	["Enhancement"] = "mdps",
+	["Holy"]		= "heal",
+	["Protection"]  = "tank",
+	["Restoration"] = "heal",
+	["Retribution"] = "mdps"
+}
 
 local tankFrames
 local mdpsFrames
@@ -44,9 +68,16 @@ local unclassifiedUnits
 -- Functions --
 ---------------
 
+--[[
+	Clasify units based on simple and obvious metrics. These might not work
+	for lesser geared people, especially in RDFs, but it's also less 
+	important in RDFs and I might even hide the entire thing in dungeons
+	and just use key combos for selecting.
+]]
 local applyBaseClasification = function(unit)
 	
 	local class = UnitLocalizedClass(unit)
+	local name, _ = UnitName(unit)
 	
 	if class == "MAGE" or class == "WARLOCK" or class == "HUNTER" then
 		table_insert(rdpsUnits, unit)
@@ -55,32 +86,46 @@ local applyBaseClasification = function(unit)
 	elseif class == "DRUID" then
 		if UnitAura(unit, "Moonkin Form") then
 			table_insert(rdpsUnits, unit)
+			nameToSpec[name] = "Balance"
 		elseif UnitAuraSource(unit, "Tree of Life") then
 			table_insert(healUnits, unit)
+			nameToSpec[name] = "Restoration"
 		elseif UnitHealthMax(unit) > 45000 then
 			table_insert(tankUnits, unit)
+			nameToSpec[name] = "Feral"
 		else
 			table_insert(unclassifiedUnits, unit)
 		end
 	elseif class == "PALADIN" then
 		if UnitPowerMax(unit) > 15000 then
 			table_insert(healUnits, unit)
+			nameToSpec[name] = "Holy"
 		elseif UnitHealthMax(unit) > 45000 then
 			table_insert(tankUnits, unit)
+			nameToSpec[name] = "Protection"
 		else
 			table_insert(mdpsUnits, unit)
+			nameToSpec[name] = "Retribution"
 		end
 	elseif class == "SHAMAN" then
 		if UnitAuraSource(unit, "Elemental Oath") then
 			table_insert(rdpsUnits, unit)
+			nameToSpec[name] = "Elemental"
 		elseif UnitPowerMax(unit) < 15000 then
 			table_insert(mdpsUnits, unit)
+			nameToSpec[name] = "Enhancement"
 		else
 			table_insert(healUnits, unit)
+			nameToSpec[name] = "Restoration"
 		end
 	elseif class == "WARRIOR" and UnitAura(unit, "Rampage") then
 		table_insert(mdpsUnits, unit)
 	elseif class == "DEATHKNIGHT" or class == "WARRIOR" then
+		--[[
+			For DK, it's a bit more tricky because they can be Blood
+			tank or dps and Frost tank or dps. Will need to see how 
+			we deal with it.
+		]]
 		if UnitHealthMax(unit) > 45000 then
 			table_insert(tankUnits, unit)
 		else 
@@ -89,8 +134,13 @@ local applyBaseClasification = function(unit)
 	elseif class == "PRIEST" then
 		if UnitAura(unit, "Shadowform") then
 			table_insert(rdpsUnits, unit)
-		else 
+			nameToSpec[name] = "Shadow"
+		else
+			--[[
+				Here could be disc or holy, will need to see how to distinguish
+			]]		
 			table_insert(unclassifiedUnits, unit)
+			nameToSpec[name] = "Discipline"
 		end
 	end
 end
@@ -184,6 +234,27 @@ end
 -- Events --
 ------------
 
+--[[
+	This is also called when raid members changed. There is no separate event for raids.
+	
+	This fires whenever someone leaves or joins. It's important to know that
+	just because someone in the party / raid is currently "raid11" doesn't 
+	mean they will stay "raid11" until the end of the party, but rather
+	change his index depending on (as far as I can tell) his position within
+	the raid list.
+	
+	The raid indices can never skip a number, even if one player is in group 1
+	and the other in group 9, their indices will be "raid1" and "raid2" 
+	respectively.
+	
+	Why this is important is because it's not possible to use SetAttribute while 
+	in combat, so the unit has to remain. From there, we have to simply adapt
+	in the moment and suddenly change what is displayed at a particular slot.
+	It could definitely be the case that one guy leaves and it messes up the 
+	entire groups, mdps in rdps group, healer in tank group, etc. It is quite
+	unlikely that someone will leave in the middle of the encounter, if anything
+	probably before or after.
+]]
 local PARTY_MEMBERS_CHANGED = function()
 
 	evaluateGroup()
@@ -283,6 +354,22 @@ local handleFrameCreation = function(frameType)
 	local result = CreateFrame("Button", nil, AssiduityGroupsFrame, "SecureUnitButtonTemplate")
 	result:SetSize(BUTTON_WIDTH, BUTTON_HEIGHT)
 	
+    result:SetAttribute("unit", "player")
+    result:SetAttribute("type", "spell")
+	
+    result:SetAttribute("*helpbutton1", "heal1")
+    result:SetAttribute("*helpbutton2", "heal2")
+	
+    result:SetAttribute("spell-heal1", "Rejuvenation")
+    result:SetAttribute("ctrl-spell-heal1", "Regrowth")
+    result:SetAttribute("shift-spell-heal1", "Wild Growth")
+    result:SetAttribute("alt-spell-heal1", "Rejuvenation")
+	
+    result:SetAttribute("spell-heal2", "Lifebloom")
+    result:SetAttribute("ctrl-spell-heal2", "Nourish")
+    result:SetAttribute("shift-spell-heal2", "Remove Curse")
+    result:SetAttribute("alt-spell-heal2", "Abolish Poison")
+	
 	local background = result:CreateTexture(nil, "BACKGROUND")
 	background:SetTexture(0, 0, 0, 0.4)
 	background:SetAllPoints()
@@ -343,6 +430,7 @@ local position = function(anchored, point, origin)
 	anchored:SetPoint(anchoredPoint, origin, point)
 end
 
+rdps1  = handleFrameCreation("rdps")
 do
 	local self = AssiduityGroupsFrame
 
@@ -359,7 +447,7 @@ do
 	tankFrames = { tank1, tank2, tank3 }
 	
 	-- Should have a maximum of 10 rdps
-	local rdps1  = handleFrameCreation("rdps")
+
 	local rdps2  = handleFrameCreation("rdps")
 	local rdps3  = handleFrameCreation("rdps")
 	local rdps4  = handleFrameCreation("rdps")
